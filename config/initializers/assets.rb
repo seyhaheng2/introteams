e05fdeda4fe0830b4ee20be9b8454889f7f5e00a
seyhaheng2 <seyhaheng2@gmail.com>
2014-11-06 14:31:30 +0700

    doen

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,6 +2,24 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
+Rails.application.config.assets.precompile += %w( js/jquery-1.11.1.min.js)
+Rails.application.config.assets.precompile += %w( js/jquery.migrate.js)
+Rails.application.config.assets.precompile += %w( js/navigate.js)
+Rails.application.config.assets.precompile += %w( js/ajax-loader.js)
+Rails.application.config.assets.precompile += %w( js/jquery-ui.min.js)
+Rails.application.config.assets.precompile += %w( js/plugins-scroll.js)
+Rails.application.config.assets.precompile += %w( js/jquery.fitvids.js)
+Rails.application.config.assets.precompile += %w( js/owl.carousel.min.js)
+Rails.application.config.assets.precompile += %w( js/imagesloaded.pkgd.min.js)
+Rails.application.config.assets.precompile += %w( js/jquery.isotope.min.js)
+Rails.application.config.assets.precompile += %w( js/waypoints.min.js)
+Rails.application.config.assets.precompile += %w( js/jquery.magnific-popup.min.js)
+Rails.application.config.assets.precompile += %w( js/classie.js)
+Rails.application.config.assets.precompile += %w( js/bootstrap.min.js )
+Rails.application.config.assets.precompile += %w( js/mlpushmenu.js)
+Rails.application.config.assets.precompile += %w( js/randomBlue.js)
+Rails.application.config.assets.precompile += %w( js/scripts.js)
+Rails.application.config.assets.precompile += %w( js/switcher.js)
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
